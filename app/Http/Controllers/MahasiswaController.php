d92060bebdc80489b16381abbeb8cb4a93f2e22d
IreneSusanto <165566757+IreneSusanto@users.noreply.github.com>
2024-11-07 23:17:34 +0700

    Add files via upload

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -10,12 +10,7 @@ use App\Models\Comment as CommentModel;
 class MahasiswaController extends Controller
 {
     //
-    public function index($nilai){
-        $arrMahasiswa = ['Risa Lestari', 'Rudi Hermawan','Bambang Kusumo','Lisa Permata'];
-        return view('universitas.mahasiswa', ['mahasiswa'=>$arrMahasiswa, 'jurusan'=>'Teknik Informatika'])
-        ->with('nilai',$nilai);
-    }
-
+   
     public function tampil(){
         $data = ProfilModel::all();
         printf($data);
